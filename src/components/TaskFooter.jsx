007635f2beb573cccb92102b54ba25ab5ede3b69
johnjanieltr <troyajohn17@gmail.com>
2024-06-20 16:37:00 -0400

    feat: add task footer component

--- src/components/TaskFooter.jsx
+++ src/components/TaskFooter.jsx
@@ -1,9 +1,13 @@
 const TaskFooter = () => {
   return (
-    <div className="mb-12">
-      TaskFooter
+    <div className=" flex justify-center mt-auto mb-12 py-3 px-5 rounded-md bg-lightWhite">
+      <div className="flex justify-between w-full max-w-44 text-base font-bold">
+        <button type="button">All</button>
+        <button type="button">Active</button>
+        <button type="button">Completed</button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default TaskFooter;
