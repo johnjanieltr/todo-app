660051b86aa3003a60cfa3d98f093e73aaf826b1
johnjanieltr <troyajohn17@gmail.com>
2024-06-20 09:24:33 -0400

    feat: add task form

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <AppBackground />
-      <main className="absolute flex flex-col top-0 left-0 right-0 bottom-0 m-auto w-85p h-[88vh]">
+      <main className="absolute flex flex-col top-0 left-0 right-0 bottom-0 m-auto w-85p h-[88vh] min-w-56 max-w-lg font-josefin">
         <TaskHeader />
         <TaskForm />
         <TaskList />
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 const TaskList = () => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+    setTask("");
+  };
+
   return (
     <div className="">
-      task form
+      <form
+        className="flex items-center py-3 px-5 bg-lightWhite rounded-md"
+        onSubmit={handleSubmit}
+      >
+        <span className="block w-4 h-4 mr-3 border border-black rounded-full"></span>
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          className="outline-0 text-sm bg-inherit grow"
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
