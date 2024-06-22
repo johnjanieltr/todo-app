1749b6ea80a8025c3897bbf7db97b2f33baf2966
johnjanieltr <troyajohn17@gmail.com>
2024-06-22 08:08:14 -0400

    chore: add custom colors to tailwind.config.js
    Added new custom colors to the Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,38 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          600: "#3a82f6", // Bright Blue
+        },
+        light: {
+          gray: {
+            50: "#fafafa", // Very Light Gray
+          },
+          grayishBlue: {
+            50: "#e3e4f1", // Very Light Grayish Blue
+            100: "#d2d3db", // Light Grayish Blue
+            400: "#96989b", // Dark Grayish Blue
+            800: "#4b5660", // Very Dark Grayish Blue
+          },
+        },
+        dark: {
+          blue: {
+            50: "#e3e4f1", // Light Grayish Blue (hover)
+            100: "#d8dee9", // Light Grayish Blue
+            500: "#68757d", // Dark Grayish Blue
+            825: "#3a3b41", // Very Dark Grayish Blue
+            800: "#252c35", // Very Dark Desaturated Blue
+            850: "#2a2c32", // Very Dark Grayish Blue (additional)
+            900: "#181820", // Very Dark Blue
+          },
+        },
+        check: {
+          background: "linear-gradient(#2ceeff, #d67eff)", // Check Background
+        },
+      },
       fontFamily: {
         josefin: ["Josefin Sans", "sans-serif"],
-      },
-      colors: {
-        lightWhite: "#fafafa",
-        darkBlue: "#161722",
       },
       width: {
         "85p": "85%",
