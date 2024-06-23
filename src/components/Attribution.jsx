6e8b059494cfd94a473089d6de79d7cb22890077
johnjanieltr <troyajohn17@gmail.com>
2024-06-23 08:58:24 -0400

    style: apply styles in Attibution component

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,16 +1,30 @@
 const Attribution = () => {
   return (
-    <div className="bg-light-gray-50">
-      Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">Your Name Here</a>.
+    <div className="py-4 px-4 text-xs text-center bg-light-gray-50">
+      <span className="block">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#3e52a3" }}
+        >
+          Frontend Mentor.
+        </a>
+      </span>
+      <span className="block">
+        Coded by{" "}
+        <a
+          href="https://github.com/johnjanieltr"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#3e52a3" }}
+        >
+          John Janiel Tr.
+        </a>
+      </span>
     </div>
   );
 };
 
 export default Attribution;
-
-// .attribution { font-size: 11px; text-align: center; }
-//     .attribution a { color: hsl(228, 45%, 44%); }
